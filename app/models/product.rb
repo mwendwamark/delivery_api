3dea73e78b17eda6b0070505d05249033032e662
mwendwamark <mwendwamark310@gmail.com>
2025-06-20 11:47:28 +0300

    add dependent destroy to the products to delete the product variants

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :product_variants
+  has_many :product_variants, dependent: :destroy
   has_many :cart_items
   has_many :order_items
   has_one_attached :image # This is the crucial line for Active Storage
